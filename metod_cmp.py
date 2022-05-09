34759b25d76f6e9607a55a616495d8922b1c3b15
DavidZahour <102311393+DavidZahour@users.noreply.github.com>
2022-05-09 20:13:21 +0200

    found centroids and add kalman filter

--- metod_cmp.py
+++ metod_cmp.py
@@ -18,25 +18,9 @@ import os
 import numpy as np
 import tensorflow as tf
 from cvat_manipulator import Cvat_manipulator
-import argparse
-import imutils
-import cv2
-import numpy as np
-from scipy.ndimage.measurements import label
-import os
-import cv2
-from tqdm import tqdm
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from skimage.io import imread
-import matplotlib.pyplot as plt
-from skimage.segmentation import mark_boundaries
-from skimage.measure import label, regionprops
-from skimage.util._montage import montage
-from collections import deque
+
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 img_size =(800,800)
-
 
 
 if __name__ == '__main__':
@@ -50,8 +34,7 @@ if __name__ == '__main__':
     model.load_weights('data/weightsfile.h5')
 
     zero_matrix = np.zeros((1, 800, 800))
-    #image = Image.open("C:\\Users\\David\\PycharmProjects\\bakalarka\\data\\dataset_1\\segmentation_dataset_2\\images\\frame_000341.PNG")
-    image =  Image.open("C:\\Users\\David\\PycharmProjects\\bakalarka\\data\\test.png")
+    image = Image.open("C:\\Users\\David\\PycharmProjects\\bakalarka\\data\\dataset_1\\segmentation_dataset_2\\images\\frame_000341.PNG")
     image = image.resize(img_size)
     data = np.asarray(image).astype('float32')
 
@@ -60,20 +43,8 @@ if __name__ == '__main__':
 
     mask = np.argmax(x, axis=-1)
     mask = np.expand_dims(mask, axis=-1)
-    x = keras.preprocessing.image.array_to_img(mask)
-    plt.imshow(x, interpolation='nearest')
-    plt.show()
     img = PIL.ImageOps.autocontrast(keras.preprocessing.image.array_to_img(mask))
-
 
     plt.imshow(img, interpolation='nearest')
     plt.imshow(image, interpolation='nearest', alpha=0.6)
     plt.show()
-
-
-
-    a = mask.reshape((800,800))
-    lbl_0 = label(mask)
-    props = regionprops(lbl_0)
-    for prop in props:
-        print('Found bbox', prop.centroid)
